5079a43f56d3d461ac063aee523603e8482297c9
Turbo Defender <rishimenon82004@gmail.com>
2020-12-18 22:28:57 +0530

    made login.html and removed temp folder

--- admins/static/template.js
+++ admins/static/template.js
@@ -1,6 +1,7 @@
 var sidebarIsClosed = false;
 
 function closeSidebar() {
+    var navItems = document.getElementsByClassName("nav-link");
     //if the sidebar is not closed
     if(!sidebarIsClosed){
         //then we close the sidebar
@@ -8,6 +9,10 @@ function closeSidebar() {
         document.getElementById('root-grid').setAttribute("style", "grid-template-columns: 0% auto");
         document.getElementById('side-bar-title').style.opacity="0%";
         document.getElementById('side-bar-title').style.fontSize="0vw";
+        for (i = 0; i < navItems.length; i++) {
+            navItems.item(i).style.opacity="0%";
+            navItems.item(i).style.fontSize="0vw";
+        }
         sidebarIsClosed = true;
     } else {
         //we open the sidebar
@@ -15,6 +20,10 @@ function closeSidebar() {
         document.getElementById('root-grid').setAttribute("style", "grid-template-columns: 18% auto");
         document.getElementById('side-bar-title').style.opacity="100%";
         document.getElementById('side-bar-title').style.fontSize="1.75vw";
+        for (i = 0; i < navItems.length; i++) {
+            navItems.item(i).style.opacity="100%";
+            navItems.item(i).style.fontSize="1.5vw";
+        }
         sidebarIsClosed = false;
     }
 }
