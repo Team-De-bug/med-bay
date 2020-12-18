4ea7b1f89fcd53f5f3e70cb4c2ca8e91e8092e4f
Marudhu Paandian K <marudhupaandian@gmail.com>
2020-12-18 10:36:17 +0530

    added cases and prescribtions.

--- patients/models.py
+++ patients/models.py
@@ -1,15 +1,32 @@
 from django.db import models
-
-blood_types = (("A+", "A+"), ("B+", "B+"), ("O+", "O+"), ("AB+", "AB+"),
-               ("A-", "A-"), ("B-", "B+"), ("O-", "O+"), ("AB-", "AB-"))
-gender_list = (("m", "male"), ("f", "female"), ("t", "trans"))
+from doctors.models import Doctor
 
 
 # Patient model.
 class Patient(models.Model):
+    # Choice list
+    blood_types = (("A+", "A+"), ("B+", "B+"), ("O+", "O+"), ("AB+", "AB+"),
+                   ("A-", "A-"), ("B-", "B+"), ("O-", "O+"), ("AB-", "AB-"))
+    gender_list = (("m", "male"), ("f", "female"), ("t", "trans"))
+
+    # Fields
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(max_length=20, choices=gender_list)
     blood_type = models.CharField(max_length=5, choices=blood_types)
     email = models.EmailField()
     phone = models.CharField(max_length=10, default=None)
+
+
+# Created when a visit is done
+class Cases(models.Model):
+    # Choices list
+    states = (("e", "Emergency"), ("n", "Normal"), ("c", "Checkup"))
+    status_options = (("t", "ToDo"), ("i", "in progress"), ("d", "done"))
+
+    # Fields
+    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
+    desc = models.TextField()
+    state = models.CharField(max_length=3, choices=states)
+    status = models.CharField(max_length=3, choices=status_options)
--- pharma/models.py
+++ pharma/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from patients.models import Cases
 
 
 # Create your models here.
@@ -7,3 +8,10 @@ class Stock(models.Model):
     desc = models.TextField()
     price = models.IntegerField()
     quantity = models.IntegerField(default=0)
+
+
+# Prescription
+class Prescription(models.Model):
+    case = models.ForeignKey(Cases, on_delete=models.SET_NULL, null=True)
+    medicines = models.ManyToManyField(Stock)
+    status = models.BooleanField(default=False)
