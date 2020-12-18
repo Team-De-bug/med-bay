987722e38d6d390487f8c0270a0fb24a840fc681
Marudhu Paandian K <marudhupaandian@gmail.com>
2020-12-18 11:34:50 +0530

    added urls for admins(staff). routed login to staff/login.

--- admins/views.py
+++ admins/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Edit the function below.
+def login(request):
+    return HttpResponse("Login page")
