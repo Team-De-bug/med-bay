c562fa4c86b9867d4e96d474555eba41906f3010
Turbo Defender <rishimenon82004@gmail.com>
2020-12-20 02:10:23 +0530

    Revert "styled prescription.css"
    This reverts commit 5712efb046a724efef9fe2d5d9a4cac2fa17e92b.

--- admins/static/template.js
+++ admins/static/template.js
@@ -5,8 +5,8 @@ function closeSidebar() {
     //if the sidebar is not closed
     if (!sidebarIsClosed) {
         //then we close the sidebar
-        document.getElementById('floating-bar').setAttribute("style", "grid-template-columns: 0% auto");
-        document.getElementById('root-grid').setAttribute("style", "grid-template-columns: 0% auto");
+        document.getElementById('floating-bar').setAttribute("style", "grid-template-columns: 0% 100%");
+        document.getElementById('root-grid').setAttribute("style", "grid-template-columns: 0% 100%");
         document.getElementById('side-bar-title').style.opacity = "0%";
         document.getElementById('side-bar-title').style.fontSize = "0vw";
         for (i = 0; i < navItems.length; i++) {
@@ -16,8 +16,8 @@ function closeSidebar() {
         sidebarIsClosed = true;
     } else {
         //we open the sidebar
-        document.getElementById('floating-bar').setAttribute("style", "grid-template-columns: 18% auto");
-        document.getElementById('root-grid').setAttribute("style", "grid-template-columns: 18% auto");
+        document.getElementById('floating-bar').setAttribute("style", "grid-template-columns: 18% 82%");
+        document.getElementById('root-grid').setAttribute("style", "grid-template-columns: 18% 82%");
         document.getElementById('side-bar-title').style.opacity = "100%";
         document.getElementById('side-bar-title').style.fontSize = "1.75vw";
         for (i = 0; i < navItems.length; i++) {
