dd16dbfa96db0254e92eefacf5041dc0923a2c10
Marudhu Paandian K <marudhupaandian@gmail.com>
2020-12-19 14:35:28 +0530

    Linked the login page. overrided the default auth form, view

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="login"),
+    path("login", views.LoginView.as_view(template_name="admins/login.html"), name="login"),
+    path("", views.home, name="home"),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
+from .forms import AuthForm
 
 
 # Edit the function below.
 def home(request):
     return render(request, "admins/home.html")
+
+
+# login page
+class LoginView(auth_views.LoginView):
+    form_class = AuthForm
