a7b4ccc3579535585b5b290b58a724827994fcff
Marudhu Paandian K <marudhupaandian@gmail.com>
2020-12-19 20:02:26 +0530

    added case list to doctor home page. made sure the user is a doctor to check the page.

--- doctors/views.py
+++ doctors/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+@login_required()
 def appointment(request):
-    return render(request, 'doctors/appointments.html')
+    user = User.objects.filter(username=request.user)[0]
+    if user.staff.role != "d":
+        return HttpResponseForbidden(HttpResponse("<h1>Only for doctors</h1>"))
+
+    cases = user.staff.doctor.cases_set.filter(status="t")
+    print(cases)
+    return render(request, 'doctors/appointments.html', context={"cases": cases})
--- med_bay/settings.py
+++ med_bay/settings.py
@@ -123,4 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home"
