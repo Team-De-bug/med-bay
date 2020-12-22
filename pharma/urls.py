7ff7aa62f5a34c21f023b07d100535f622079d97
shabesa <k.a.shabesa@gmail.com>
2020-12-22 17:27:20 +0530

    Added increse quantity fuction to cart page

--- pharma/urls.py
+++ pharma/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.shop, name="shop"),
-    path("place", views.place, name="place"),
+    path("place", views.place, name="shop"),
+    path("remove", views.remove, name="cart"),
+    path("add_one", views.add_one, name="cart"),
     path("cart", views.cart, name="cart")
 ]
