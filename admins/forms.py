5d06fb0c513607aa80e9eedf9262852f5bf273a7
Turbo Defender <rishimenon82004@gmail.com>
2020-12-23 20:59:38 +0530

    added BillForm

--- admins/forms.py
+++ admins/forms.py
@@ -4,14 +4,14 @@ from django import forms
 
 # Overriding the default authentication form
 class AuthForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={"class": "login-input",
+    username = UsernameField(widget=forms.TextInput(attrs={"class": "form-input",
                                                            "id": "username",
                                                            'placeholder': 'username',
                                                            'autofocus': ''}))
     password = forms.CharField(
         strip=False,
         widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                          'class': 'login-input',
+                                          'class': 'form-input',
                                           'id': 'password',
                                           'placeholder': 'password',
                                           'autofocus': ''}))
--- pharma/forms.py
+++ pharma/forms.py
@@ -15,3 +15,16 @@ class StockForm(forms.Form):
                                                                'id':'amount',
                                                                'name':'quantity',
                                                                'placeholder':'100'}))
+
+class BillForm(forms.Form):
+    name =  forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input',
+                                                           'id': 'name',
+                                                           'placeholder': 'Name',
+                                                           'autofocus': ''}))
+    
+    phone = forms.CharField(widget=forms.NumberInput(attrs={'class': "form-input",
+                                                            'id': 'phone',
+                                                            'placeholder': '1234567890',
+                                                            'min': 1000000000,
+                                                            'max': 9999999999,
+                                                            'autofocus': ''}))
