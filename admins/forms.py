69e35e46f9237f6b7d2c0d6ed3cb7d16fb072ba4
Turbo Defender <rishimenon82004@gmail.com>
2020-12-25 01:37:24 +0530

    added ajax to orders.html and added min max to phone number to limit it to 10 digits

--- admins/forms.py
+++ admins/forms.py
@@ -5,8 +5,8 @@ from patients.models import Patient
 
 # Overriding the default authentication form
 class AuthForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={"class": "form-input",
-                                                           "id": "username",
+    username = UsernameField(widget=forms.TextInput(attrs={'class': 'form-input',
+                                                           'id': 'username',
                                                            'placeholder': 'username',
                                                            'autofocus': ''}))
     password = forms.CharField(
@@ -24,28 +24,30 @@ class PatientForm(forms.ModelForm):
         model = Patient
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput(attrs={"class": "form-input",
-                                           "id": "name",
+            'name': forms.TextInput(attrs={'class': 'form-input',
+                                           'id': 'name',
                                            'placeholder': 'Name',
                                            'autofocus': ''}),
-            'age': forms.NumberInput(attrs={"class": "form-input",
-                                            "id": "age",
+            'age': forms.NumberInput(attrs={'class': 'form-input',
+                                            'id': 'age',
                                             'placeholder': '100',
                                             'min': 0,
                                             'max': 150,
                                             'autofocus': ''}),
-            'gender': forms.Select(choices=Patient.gender_list, attrs={"class": "form-input-dropdown",
-                                                                       "id": "gender",
+            'gender': forms.Select(choices=Patient.gender_list, attrs={'class': 'form-input-dropdown',
+                                                                       'id': 'gender',
                                                                        'autofocus': ''}),
-            'blood_type': forms.Select(choices=Patient.blood_types, attrs={"class": "form-input-dropdown",
-                                                                           "id": "b",
+            'blood_type': forms.Select(choices=Patient.blood_types, attrs={'class': 'form-input-dropdown',
+                                                                           'id': 'blood_type',
                                                                            'autofocus': ''}),
-            'email': forms.EmailInput(attrs={"class": "form-input",
-                                             "id": "email",
+            'email': forms.EmailInput(attrs={'class': 'form-input',
+                                             'id': 'email',
                                              'placeholder': 'example@email.com',
                                              'autofocus': ''}),
-            'phone': forms.NumberInput(attrs={"class": "form-input",
-                                              "id": "phone",
+            'phone': forms.NumberInput(attrs={'class': 'form-input',
+                                              'id': 'phone',
+                                              'min': 1000000000,
+                                              'max': 9999999999,
                                               'placeholder': '1234567890',
                                               'autofocus': ''})
         }
