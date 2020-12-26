45e07c69c16a2b74f518fd9cb1810780a695677f
Turbo Defender <rishimenon82004@gmail.com>
2020-12-26 21:32:29 +0530

    unified css class styling

--- doctors/forms.py
+++ doctors/forms.py
@@ -6,13 +6,13 @@ class CaseProcessing(forms.Form):
 
     id = forms.IntegerField()
 
-    has_pres = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': "patient-detail",
+    has_pres = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': "item-detail",
                                                                     'id': "has-prescription",
                                                                     'name': "has-prescription",
                                                                     'type': "checkbox",
                                                                     'default': "false"}), required=False)
 
-    desc = forms.CharField(widget=forms.Textarea(attrs={'class': "patient-detail",
+    desc = forms.CharField(widget=forms.Textarea(attrs={'class': "item-detail",
                                                         'id': "diagnosis",
                                                         'cols': "16", 'rows': "5",
                                                         'name': "diagnosis",
