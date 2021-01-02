54024a4960b3e7f0de16082d408d844565752cfc
Marudhu Paandian K <marudhupaandian@gmail.com>
2021-01-02 12:52:09 +0530

    added entries for accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Entries(models.Model):
+
+    type_choices = [(True, "income"), (False, "expense")]
+
+    date = models.DateField()
+    desc = models.TextField()
+    cat = models.CharField(max_length=32)
+    price = models.PositiveIntegerField()
+    type = models.BooleanField(choices=type_choices)
+
+    def __str__(self):
+        return f'{self.cat}, {self.desc}, {self.price}'
