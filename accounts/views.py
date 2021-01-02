0bed5bca2b4f92ad2aeb8fb987e18c3ce2f04b92
Marudhu Paandian K <marudhupaandian@gmail.com>
2021-01-02 21:04:31 +0530

    added saving functionality to entries in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from admins.utils import validate_access
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Entries
+import json
 
 
 # Create your views here.
@@ -15,10 +17,19 @@ def home(request):
 def entries(request):
     validate_access(request, 'ac')
 
+    # Handling the request to save entries
+    if 'entries' in request.GET:
+        data = json.loads(request.GET['entries'])
+        for ent in data.values():
+            t = ent[3] == 'Income'
+            entry = Entries(date=ent[0], desc=ent[1], cat=ent[2], type=t, price=int(ent[4]))
+            entry.save()
+        return HttpResponse('success')
+
+    # getting all the entries and the unique categories
     entries = Entries.objects.all()
     cats = []
     for cat in entries.values('cat').distinct():
         cats.append(cat['cat'])
-    print(cats)
 
     return render(request, 'accounts/entries.html', context={"entries": entries, "cats": cats})
